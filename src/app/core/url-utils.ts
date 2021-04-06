30406a674023d125910332158aa49f4650240abf
bliulin <bliulinx@gmail.com>
2021-04-06 18:24:58 +0300

    remove

--- src/app/core/url-utils.ts
+++
@@ -1,7 +0,0 @@
-export function getQueryParam(uri: string, name: string): string {
-    const results = new RegExp('[\\?&]' + name + '=([^&#]*)').exec(uri);
-    if (!results) {
-        return null;
-    }
-    return results[1];
-}
